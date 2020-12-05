0da58579419906bdd447a530fa80b157151c43e0
AmirO8 <AmirO8@users.noreply.github.com>
2020-12-04 19:40:20 -0800

    Adding PyPoll.py file.

---
+++ KD Practice.py
@@ -0,0 +1,20 @@
+#What is the score?
+kd = float(input("What is your kd?"))
+
+# Determine the grade.
+if kd >= 1.3:
+    print('Your kd is comprable to Alex')
+elif kd >= 1.2:
+    print('Your kd is comprable to Zach')
+elif kd >= 1.1:
+    print('Your kd is comprable to Ian')
+elif kd >= 1.0:
+    print('Your kd is comprable to Jupes')
+elif kd >= .9:
+    print('Your kd is comprable to Dane')
+elif kd >= .8:
+    print('Your kd is comprable to Amir')
+elif kd >= .7:
+    print('Your kd is comprable to Vlad')
+else:
+    print('You Fucking Suck Balls')
---
+++ Test.py
@@ -0,0 +1,24 @@
+#What is your KD
+KD = int((input("What is your KD"))
+
+#Determine who you're comprable to
+if KD >= 8:
+    print('Your KD is comprable to Amir')
+elif KD >= 9:
+    print('Your KD is comprable to Dane')
+elif KD >= 10:
+    print('Your KD is comprable to Jupes')
+elif KD >= 11:
+    print('Your KD is comprable to Zach')
+elif KD >= 12:
+    print('Your KD is comprable to Alex')
+
+
+    
+
+
+
+
+
+
+
